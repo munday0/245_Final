f6775c84777de8374b9be5906b2a46878cefea0d
Isa Lee <isalee@chapman.edu>
2021-05-08 21:47:56 -0700

    added all characters and skins

--- CPSC245Final-CharacterSelect/Assets/Scripts/Character.cs
+++ CPSC245Final-CharacterSelect/Assets/Scripts/Character.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/*Stephanie Munday
- * ID: 2343625
- * smunday@chapman.edu
+/* 
+ * Stephanie Munday, Isa Lee
+ * ID: 2343625, 2343486
+ * smunday@chapman.edu, isalee@chapman.edu
  * CPSC 245-01
  * Final - Character Select
  *
@@ -13,103 +14,118 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    public GameObject[] Characters = new GameObject[4]; //list of different character objects to be chosen from
+    public GameObject[] Characters = new GameObject[24]; //list of different character objects to be chosen from
 
     public int CharaIndex = 0; //index of character object
+    private int currIndex;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //InstantiateCharacter();
-    }
-
+    // Alternates between character skins
     public void SwitchCharacter()
     {
-        Characters[CharaIndex].SetActive(false);
-        CharaIndex = (CharaIndex + 1) % Characters.Length;
+        ClearModels();
+        if (CharaIndex % 2 == 0)
+        {
+            CharaIndex = (currIndex + 1) % Characters.Length;
+        }
+        else
+        {
+            CharaIndex = (currIndex) % Characters.Length;
+        }
         Characters[CharaIndex].SetActive(true);
+    }
+
+    // Clear visible characters 
+    public void ClearModels()
+    {
+        for(int i = 0; i < Characters.Length; ++i)
+        {
+            Characters[i].SetActive(false);
+        }
+    }
+
+    // Displays character at given index
+    public void SetModel(int index)
+    {
+        ClearModels();
+        Characters[index].SetActive(true);
+        currIndex = index;
+    }
+
+    // Spawns set character
+    public void CharacterOne()
+    {
+        CharaIndex = 0;
+        SetModel(CharaIndex);
+    }
+
+    public void CharacterTwo()
+    {
+        CharaIndex = 2;
+        SetModel(CharaIndex);
+    }
+
+    public void CharacterThree()
+    {
+        CharaIndex = 4;
+        SetModel(CharaIndex);
+    }
+
+    public void CharacterFour()
+    {
+        CharaIndex = 6;
+        SetModel(CharaIndex);
+    }
+
+    public void CharacterFive()
+    {
+        CharaIndex = 8;
+        SetModel(CharaIndex);
+    }
+
+    public void CharacterSix()
+    {
+        CharaIndex = 10;
+        SetModel(CharaIndex);
+    }
+
+    public void CharacterSeven()
+    {
+        CharaIndex = 12;
+        SetModel(CharaIndex);
+    }
+
+    public void CharacterEight()
+    {
+        CharaIndex = 14;
+        SetModel(CharaIndex);
+    }
+
+    public void CharacterNine()
+    {
+        CharaIndex = 16;
+        SetModel(CharaIndex);
+    }
+
+    public void CharacterTen()
+    {
+        CharaIndex = 18;
+        SetModel(CharaIndex);
+    }
+
+    public void CharacterEleven()
+    {
+        CharaIndex = 20;
+        SetModel(CharaIndex);
+    }
+
+    public void CharacterTwelve()
+    {
+        CharaIndex = 22;
+        SetModel(CharaIndex);
     }
 
     public void StartGame()
     {
         PlayerPrefs.SetInt("CharaIndex", CharaIndex);
     }
-
-
-
-    //public void InstantiateCharacter()
-    //{
-    //    Instantiate(Characters[CharaIndex], transform.position, transform.rotation, transform);
-    //    Debug.Log("Chara Index:" + CharaIndex);
-    //}
-
-    //public void ChangeCharacter() //6:16
-    //{
-    //    for (int i = 0; i < Characters.Length; i++)
-    //    {
-    //        if (i == CharaIndex)
-    //        {
-    //            Characters[i].SetActive(true);
-    //        }
-    //        else
-    //        {
-    //            Characters[i].SetActive(false);
-    //        }
-    //    }
-    //}
-
-
-    //public void SelectF1()
-    //{
-    //    CharaIndex = 0;
-    //    //InstantiateCharacter();
-    //    ChangeCharacter();
-    //    Debug.Log("F1");
-    //}
-
-    //public void SelectF2()
-    //{
-    //    CharaIndex = 1;
-    //    //InstantiateCharacter();
-    //    ChangeCharacter();
-    //    Debug.Log("F2");
-    //}
-
-    //public void SelectM1()
-    //{
-    //    CharaIndex = 2;
-    //    //InstantiateCharacter();
-    //    ChangeCharacter();
-    //    Debug.Log("M1");
-    //}
-
-    //public void SelectM2()
-    //{
-    //    CharaIndex = 3;
-    //    //InstantiateCharacter();
-    //    ChangeCharacter();
-    //    Debug.Log("M2");
-    //}
-
-    //void Update()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.Alpha1))
-    //    {
-    //        SelectF1();
-    //    }
-    //    if (Input.GetKeyDown(KeyCode.Alpha2))
-    //    {
-    //        SelectF2();
-    //    }
-    //    if (Input.GetKeyDown(KeyCode.Alpha3))
-    //    {
-    //        SelectM1();
-    //    }
-    //    if (Input.GetKeyDown(KeyCode.Alpha4))
-    //    {
-    //        SelectM2();
-    //    }
-    //}
-
-
 }
