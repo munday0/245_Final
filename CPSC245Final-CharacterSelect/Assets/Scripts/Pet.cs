5de1a6c4d9b4d8ec044405b7d2e417a1a3d1f3dd
Isa Lee <isalee@chapman.edu>
2021-05-10 10:56:40 -0700

    added button panel

--- CPSC245Final-CharacterSelect/Assets/Scripts/Pet.cs
+++ CPSC245Final-CharacterSelect/Assets/Scripts/Pet.cs
@@ -2,8 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* 
+* Stephanie Munday, Isa Lee
+* ID: 2343625, 2343486
+* smunday@chapman.edu, isalee@chapman.edu
+* CPSC 245-01
+* Final - Character Select
+*
+* Pet script - accounts for switching between pets
+*/
+
 public class Pet : MonoBehaviour
 {
+    public GameObject[] Pets = new GameObject[24]; //list of different character objects to be chosen from
     // Start is called before the first frame update
     void Start()
     {
