19453ac868384c0cd5e2531594301e1254540bb9
Isa Lee <isalee@chapman.edu>
2021-05-12 10:52:25 -0700

    added button selection outline

---
+++ CPSC245Final-CharacterSelect/Assets/Scripts/SelectionTracker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionTracker : MonoBehaviour
+{
+    public Button b; // active button
+
+    // activate (pass in), deactivate, updateActive(pass in clicked button) (assign to onClick + variable)
+    // all buttons outline --> click (deactivate and update)
+    // .setActive (go)--> .enabled (com)
+    // default select
+
+    void setColor()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
