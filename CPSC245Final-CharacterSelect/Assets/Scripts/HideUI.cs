1effff119b10364cdf7e827ece2325f3612e12da
Isa Lee <isalee@chapman.edu>
2021-05-18 23:24:21 -0700

    added done screen + name display

---
+++ CPSC245Final-CharacterSelect/Assets/Scripts/HideUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideUI : MonoBehaviour
+{
+    private CanvasGroup c;
+
+    private void Start()
+    {
+        c = GetComponent<CanvasGroup>();
+    }
+
+    public void Hide()
+    {
+        c.alpha = 0f;
+        c.blocksRaycasts = false;
+        c.interactable = false;
+    }
+
+    public void Show()
+    {
+        c.alpha = 1f;
+        c.blocksRaycasts = true;
+        c.interactable = true;
+    }
+}
---
+++ CPSC245Final-CharacterSelect/Assets/Scripts/NameUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameUI : MonoBehaviour
+{
+    public string CharacterName;
+    public string PetName;
+    public GameObject CharacterInputField;
+    public GameObject PetInputField;
+    public GameObject CharacterTextDisplay;
+    public GameObject PetTextDisplay;
+
+    public void DisplayName()
+    {
+        //Debug.Log("In DisplayName");
+        CharacterName = CharacterInputField.GetComponent<Text>().text;
+        PetName = PetInputField.GetComponent<Text>().text;
+        CharacterTextDisplay.GetComponent<Text>().text = CharacterName;
+        PetTextDisplay.GetComponent<Text>().text = PetName;
+        //Canvas.transform.Find("CharacterNameInput/Text").GetComponent<Text>();
+        //PetName = Canvas.transform.Find("PetNameInput/Text").GetComponent<Text>();
+        //CharacterName.text = "check1";
+        //PetName.text = "sadness";
+        //string charName = CharacterName.text.ToString();
+        //string petName = PetName.text.ToString();
+    }
+}
