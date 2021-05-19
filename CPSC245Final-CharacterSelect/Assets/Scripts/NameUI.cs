012bcce1ce0f842d73bd0f3b37dd6d364641d7e5
Isa Lee <isalee@chapman.edu>
2021-05-18 23:38:49 -0700

    updated formatting

--- CPSC245Final-CharacterSelect/Assets/Scripts/NameUI.cs
+++ CPSC245Final-CharacterSelect/Assets/Scripts/NameUI.cs
@@ -14,16 +14,9 @@ public class NameUI : MonoBehaviour
 
     public void DisplayName()
     {
-        //Debug.Log("In DisplayName");
         CharacterName = CharacterInputField.GetComponent<Text>().text;
         PetName = PetInputField.GetComponent<Text>().text;
         CharacterTextDisplay.GetComponent<Text>().text = CharacterName;
         PetTextDisplay.GetComponent<Text>().text = PetName;
-        //Canvas.transform.Find("CharacterNameInput/Text").GetComponent<Text>();
-        //PetName = Canvas.transform.Find("PetNameInput/Text").GetComponent<Text>();
-        //CharacterName.text = "check1";
-        //PetName.text = "sadness";
-        //string charName = CharacterName.text.ToString();
-        //string petName = PetName.text.ToString();
     }
 }
